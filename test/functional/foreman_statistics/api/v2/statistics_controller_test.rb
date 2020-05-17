ff3733b593acf004c630549ed5fa04acffca7440
Ondřej Ezr <oezr@redhat.com>
2020-05-17 02:02:12 +0200

    Add statistics controllers

---
+++ test/functional/foreman_statistics/api/v2/statistics_controller_test.rb
@@ -0,0 +1,19 @@
+require_relative '../../../../test_plugin_helper'
+
+class ForemanStatistics::Api::V2::StatisticsControllerTest < ActionController::TestCase
+  setup do
+    @routes = ForemanStatistics::Engine.routes
+  end
+
+  test "should get statistics" do
+    get :index
+    assert_response :success
+    response = ActiveSupport::JSON.decode(@response.body)
+    assert_not response.empty?
+    expected_keys = ["arch_count", "cpu_count", "env_count", "klass_count",
+                     "mem_free", "mem_size", "mem_totfree", "mem_totsize",
+                     "model_count", "os_count", "swap_free", "swap_size"]
+
+    assert_equal expected_keys, response.keys.sort
+  end
+end
