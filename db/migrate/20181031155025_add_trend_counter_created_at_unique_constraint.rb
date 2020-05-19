849974e6c18206d8865e7849747dbc9befe26be1
Ondřej Ezr <oezr@redhat.com>
2020-05-19 15:06:23 +0200

    Remove Trend data migrations

--- db/migrate/20181031155025_add_trend_counter_created_at_unique_constraint.rb
+++ db/migrate/20181031155025_add_trend_counter_created_at_unique_constraint.rb
@@ -1,18 +1,5 @@
 class AddTrendCounterCreatedAtUniqueConstraint < ActiveRecord::Migration[6.0]
   def up
-    fields = %i[trend_id created_at]
-    duplicates = TrendCounter.order(created_at: :asc).select(fields)
-                             .group(*fields)
-                             .having(TrendCounter.arel_table[:created_at].count.gt(1))
-                             .pluck(*fields)
-
-    duplicates.each do |duplicate|
-      trend_id, created_at = duplicate
-      to_delete = TrendCounter.where(trend_id: trend_id, created_at: created_at).pluck(:id)[1..-1]
-      say "Removing duplicate TrendCounters for Trend #{trend_id} with IDs: #{to_delete.to_sentence}"
-      TrendCounter.where(id: to_delete).delete_all
-    end
-
     add_index :trend_counters, %i[trend_id created_at], unique: true
   end
 
