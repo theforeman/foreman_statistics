d5623c5a0d090cb228097ca29f07f2828affa146
Ondřej Ezr <oezr@redhat.com>
2020-07-13 02:46:52 +0200

    Fix migrations missing Trend model

--- db/migrate/20200605153005_migrate_core_types.rb
+++ db/migrate/20200605153005_migrate_core_types.rb
@@ -1,15 +1,19 @@
 class MigrateCoreTypes < ActiveRecord::Migration[6.0]
+  class FakeTrend < ApplicationRecord
+    self.table_name = 'trends'
+  end
+
   def up
     Permission.where(:resource_type => 'Trend').update_all(:resource_type => 'ForemanStatistics::Trend')
     %w[ForemanTrend FactTrend Trend].each do |t|
-      Trend.where(:type => t).update_all(:type => "ForemanStatistics::#{t}")
+      FakeTrend.where(:type => t).update_all(:type => "ForemanStatistics::#{t}")
     end
   end
 
   def down
     Permission.where(:resource_type => 'ForemanStatistics::Trend').update_all(:resource_type => 'Trend')
     %w[ForemanTrend FactTrend Trend].each do |t|
-      Trend.where(:type => "ForemanStatistics::#{t}").update_all(:type => t)
+      FakeTrend.where(:type => "ForemanStatistics::#{t}").update_all(:type => t)
     end
   end
 end
