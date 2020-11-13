0fe62f5ead98433d024acddee638bbffbc507dba
Ondřej Ezr <oezr@redhat.com>
2020-11-13 13:01:51 +0100

    Use global React router

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ ForemanStatistics::Engine.routes.draw do
   end
 
   resources :statistics, :only => %i[index show], constraints: ->(req) { req.format == :json }
-  match 'statistics' => 'react#index', :via => :get
+  match 'statistics' => '/react#index', :via => :get
 
   namespace :api, defaults: { format: 'json' } do
     scope '(:apiv)', module: :v2, defaults: { apiv: 'v2' }, apiv: /v1|v2/, constraints: ApiConstraints.new(version: 2, default: true) do
