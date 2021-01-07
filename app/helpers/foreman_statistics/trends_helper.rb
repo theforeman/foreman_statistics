4fdb5dc8d5aac52354ae13199d7ef81682da8915
Ondřej Ezr <oezr@redhat.com>
2021-01-07 22:07:06 +0100

    Fixes #31594 - reactify trends chart

--- app/helpers/foreman_statistics/trends_helper.rb
+++ app/helpers/foreman_statistics/trends_helper.rb
@@ -12,7 +12,7 @@ module ForemanStatistics
     def trend_days_filter(trend)
       form_tag trend, :id => 'days_filter', :method => :get, :class => 'form form-inline' do
         content_tag(:span, (_('Trend of the last %s days.') %
-                            select(nil, 'range', 1..Setting[:max_trend], { :selected => range },
+                            select(nil, 'range', 1..Setting[:max_trend], { :selected => trends_range },
                               { :onchange => "$('#days_filter').submit();$(this).attr('disabled','disabled');;" })).html_safe)
       end
     end
@@ -25,28 +25,40 @@ module ForemanStatistics
       end
     end
 
-    def chart_data(trend, from = Setting[:max_trend], _to = Time.now.utc)
-      chart_colors = ['#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE', '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92']
-      values = trend.values
-      labels = {}
-      values.includes(:trendable).each { |v| labels[v.id] = [CGI.escapeHTML(v.to_label), trend_path(:id => v)] }
-      values.includes(:trend_counters).where(['trend_counters.interval_end > ? or trend_counters.interval_end is null', from])
-            .reorder('trend_counters.interval_start')
-            .each_with_index.map do |value, idx|
-        data = []
+    ##
+    # Returns data in format:
+    #
+    # [
+    #   [time, <time_int>, <time_int>],
+    #   [trend_val1, <host_count>, <host_count>],
+    #   [trend_val2, 5, 2],
+    #   [trend_valx, 213, 3]
+    # ]
+    def trend_chart_data(trend, from = Setting[:max_trend])
+      data = {}
+      names = {}
+      paths = {}
+      trend.values.preload(:trendable).each do |value|
+        names[value.id] = CGI.escapeHTML(value.to_label)
+        paths[value.id] = trend_path(id: value)
+      end
+      trend.values.preload(:trend_counters).joins(:trend_counters)
+           .where(['trend_counters.interval_end > ? or trend_counters.interval_end is null', from])
+           .reorder('trend_counters.interval_start')
+           .each do |value|
         value.trend_counters.each do |counter|
-          # cut the left side of the graph
-          interval_start = (counter.interval_start || from) > from ? counter.interval_start : from
+          current_data = data[counter.interval_start.to_i] ||= {}
           next_timestamp = counter.try(:interval_end) || Time.now.utc
-          # transform the timestamp values to flot format - from seconds in Ruby to milliseconds in flot
-          data << [interval_start.to_i * 1000, counter.count]
-          data << [next_timestamp.to_i * 1000 - 1, counter.count]
+          next_data = data[next_timestamp.to_i] ||= {}
+          current_data[value.id] = next_data[value.id] = counter.count
         end
-        { :label => labels[value.id][0], :href => labels[value.id][1], :data => data, :color => chart_colors[idx % chart_colors.size] } unless data.empty?
-      end.compact
+      end
+      times = data.keys.sort
+      result = names.map { |id, label| [label].concat(times.map { |time| data[time][id].to_f }) }
+      result.unshift(['time'].concat(times))
     end
 
-    def range
+    def trends_range
       params['range'].empty? ? Setting[:max_trend] : params['range'].to_i
     end
   end
