6f56564859bdde42658869aeeb7604e34243bee7
Ondřej Ezr <oezr@redhat.com>
2021-10-28 11:27:48 +0200

    Add the counters and patch for puppet

--- app/controllers/foreman_statistics/api/v2/statistics_controller.rb
+++ app/controllers/foreman_statistics/api/v2/statistics_controller.rb
@@ -11,11 +11,11 @@ module ForemanStatistics
 
         api :GET, '/statistics/', N_('Get statistics')
         def index
-          @os_count    = Host.authorized(:view_hosts).count_distribution :operatingsystem
-          @arch_count  = Host.authorized(:view_hosts).count_distribution :architecture
+          @os_count    = Statistics::CountHosts.new(count_by: :operatingsystem)
+          @arch_count  = Statistics::CountHosts.new(count_by: :architecture)
           if defined?(ForemanPuppet)
-            @env_count   = Host.authorized(:view_hosts).count_distribution :environment
-            @klass_count = Host.authorized(:view_hosts).count_habtm 'puppetclass'
+            @env_count   = Statistics::CountHosts.new(count_by: :environment)
+            @klass_count = Statistics::CountPuppetClasses.new(id: :puppetclass)
           end
           @cpu_count   = FactValue.authorized(:view_facts).my_facts.count_each 'processorcount'
           @model_count = FactValue.authorized(:view_facts).my_facts.count_each 'manufacturer'
@@ -25,8 +25,8 @@ module ForemanStatistics
           @swap_free   = FactValue.authorized(:view_facts).my_facts.mem_average 'swapfree'
           @mem_totsize = FactValue.authorized(:view_facts).my_facts.mem_sum 'memorysize'
           @mem_totfree = FactValue.authorized(:view_facts).my_facts.mem_sum 'memoryfree'
-          render :json => { :os_count => @os_count, :arch_count => @arch_count, :swap_size => @swap_size,
-                            :env_count => @env_count, :klass_count => @klass_count, :cpu_count => @cpu_count,
+          render :json => { :os_count => @os_count.calculate, :arch_count => @arch_count.calculate, :swap_size => @swap_size,
+                            :env_count => @env_count&.calculate, :klass_count => @klass_count&.calculate, :cpu_count => @cpu_count,
                             :model_count => @model_count, :mem_size => @mem_size, :mem_free => @mem_free,
                             :swap_free => @swap_free, :mem_totsize => @mem_totsize, :mem_totfree => @mem_totfree }
         end
